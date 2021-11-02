5afe2019e2cc04dc61bcb08718cf891995971408
ljubicamiljkovic56 <ljubica.m19@gmail.com>
2021-11-02 15:55:38 +0100

    Kontakt stranica zavrsena, dodate neke stvari

---
+++ js/login.js
@@ -0,0 +1,36 @@
+$(document).ready(function(e) {
+    $("#loginForm").submit(function(e){
+        e.preventDefault();
+
+        var username = $("input[name=username]").val().trim();
+        var password = $("input[name=password]").val().trim();
+
+        if(username == "" || password == ""){
+            alert('Niste uneli sve podatke');
+        } else {
+            $.ajax({
+                url: $("#loginForm").attr("action"),
+                type: "GET",
+                dataType: "json",
+                success: function(response) {
+                    var name = "";
+                    for(var i = 0; i<response.length, i++;) {
+                        var korisnik = response[i];
+                        if(korisnik.email == username && korisnik.username == password){
+                            name = korisnik.name;
+                            break;
+                        }
+                    }
+                    if(name == ""){
+                        alert('Neispravni podaci');
+                    }else {
+                        window.location.replace("letovi.html?user=" + name);
+                    }
+                },
+                error: function(request, error, message){
+                    alert("Greska: " + message, error, request);
+                }
+            })
+        }
+    });
+});
